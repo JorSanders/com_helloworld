0d57e5db52b04679fcfb297ab4cd6d5806f5d725
Jor Sanders <Jor Sanders>
2017-02-21 19:17:29 +0100

    added site model

---
+++ site/models/helloworld.php
@@ -0,0 +1,32 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+* HelloWorld Model
+*
+* @since  0.0.1
+*/
+class HelloWorldModelHelloWorld extends JModelItem
+{
+/**
+* @var string message
+*/
+protected $message;
+
+/**
+* Get the message
+*
+* @return  string  The message to be displayed to the user
+*/
+public function getMsg()
+{
+if (!isset($this->message))
+{
+$this->message = 'Hello World!';
+}
+
+return $this->message;
+}
+}
