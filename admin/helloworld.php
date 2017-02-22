2103cb3ec6cd546d3effc19650c4557ba50ea63e
Jor Sanders <Jor Sanders>
2017-02-22 15:19:11 +0100

    backend list for admin added

--- admin/helloworld.php
+++ admin/helloworld.php
@@ -1,1 +1,13 @@
-Hello World administration
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Get an instance of the controller prefixed by HelloWorld
+$controller = JControllerLegacy::getInstance('HelloWorld');
+
+// Perform the Request task
+$controller->execute(JFactory::getApplication()->input->get('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
