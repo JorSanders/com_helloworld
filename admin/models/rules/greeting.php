6f099fe5a24a7d3152da0c0df9348eb85567980d
Jor Sanders <jorsanders@hotmail.com>
2017-02-23 15:45:33 +0100

    client and server side verivication

---
+++ admin/models/rules/greeting.php
@@ -0,0 +1,19 @@
+<?php
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Form Rule class for the Joomla Framework.
+ */
+class JFormRuleGreeting extends JFormRule
+{
+    /**
+     * The regular expression.
+     *
+     * @access    protected
+     * @var        string
+     * @since    2.5
+     */
+    protected $regex = '^[^0-9]+$';
+}
